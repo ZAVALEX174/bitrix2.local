fdd2419f16293f00c1376cb9b1c6725c4b2fb91a
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-09-20 11:58:57 +0500

    grr

---
+++ public/example2/index.php
@@ -0,0 +1,6 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Наша супер страница");
+?>
+    hello world@@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
